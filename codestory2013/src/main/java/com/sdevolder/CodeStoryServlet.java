72c8eaf88627dd168151564e119a64e3702b7fa9
sdevolder <sebastien.devolder@gmail.com>
2013-01-11 00:58:14 +0100

    la ouverte du contenu de httpservletrequest.tostring rend obsolète la construction de mon log

--- codestory2013/src/main/java/com/sdevolder/CodeStoryServlet.java
+++ codestory2013/src/main/java/com/sdevolder/CodeStoryServlet.java
@@ -1,10 +1,6 @@
 package com.sdevolder;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,40 +11,12 @@ import org.apache.log4j.Logger;
 
 public class CodeStoryServlet extends HttpServlet {
 
+    private static final String SEBASTIEN_DEVOLDER_GMAIL_COM = "sebastien.devolder@gmail.com";
     private final Logger log = Logger.getLogger(CodeStoryServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.info("A request has arrived");
-        @SuppressWarnings("unchecked")
-        Map<String, Object> parameterMap = req.getParameterMap();
-        log.info("It contains " + parameterMap.size() + " parameters");
-        if (parameterMap.size() > 0) {
-            log.info(buildParamList(parameterMap));
-        }
-        resp.getWriter().write("sebastien.devolder@gmail.com");
-    }
-
-    protected String buildParamList(Map<String, Object> parameterMap) {
-        StringBuilder sb = new StringBuilder("Parameters list\n");
-        Set<String> keySet = parameterMap.keySet();
-        Iterator<String> iterator = keySet.iterator();
-        int index = 0;
-        while (iterator.hasNext()) {
-            index++;
-            String paramName = iterator.next();
-            sb.append("Param " + index + ": [" + paramName + "]");
-            Object value = parameterMap.get(paramName);
-            String valueString;
-            if (value instanceof String[]) {
-                valueString = "L";// Pour marquer la valeur du paramètre comme étant une liste
-                valueString += Arrays.toString(((String[]) value));
-            } else {
-                valueString = value.toString();
-            }
-            sb.append("[" + valueString + "]");
-            sb.append("\n");
-        }
-        return sb.toString();
+        log.info(req.toString());
+        resp.getWriter().write(SEBASTIEN_DEVOLDER_GMAIL_COM);
     }
 }
--- codestory2013/src/test/java/com/sdevolder/CodeStoryServletTest.java
+++ codestory2013/src/test/java/com/sdevolder/CodeStoryServletTest.java
@@ -3,13 +3,11 @@ package com.sdevolder;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.fest.assertions.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -18,9 +16,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CodeStoryServletTest {
-    private static final String TEXT1 = "Parameters list\nParam 1: [param][value]\n";
-    private static final String TEXT2 = "Parameters list\nParam 1: [q][L[ton, adresse]]\n";
-
     private final CodeStoryServlet runner = new CodeStoryServlet();
 
     @Mock
@@ -29,23 +24,6 @@ public class CodeStoryServletTest {
     HttpServletResponse httpServletResponse;
     @Mock
     PrintWriter writer;
-
-    @Test
-    public void testBuildParamListWithString() {
-        Map<String, Object> parameterMap = new HashMap<String, Object>(1);
-        parameterMap.put("param", "value");
-        Assertions.assertThat(runner.buildParamList(parameterMap)).isEqualTo(TEXT1);
-    }
-
-    @Test
-    public void testBuildParamListWithStringList() {
-        Map<String, Object> parameterMap = new HashMap<String, Object>(1);
-        String[] paramValues = new String[2];
-        paramValues[0] = "ton";
-        paramValues[1] = "adresse";
-        parameterMap.put("q", paramValues);
-        Assertions.assertThat(runner.buildParamList(parameterMap)).isEqualTo(TEXT2);
-    }
 
     @Test
     public void testGet() throws IOException, ServletException {
