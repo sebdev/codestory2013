70331b09d23bea7a1c7ddcbb448e6399127fed71
sdevolder <sebastien.devolder@gmail.com>
2013-01-18 00:23:48 +0100

    Ajout des logger des requetes et reponses http

--- codestory2013/src/main/java/com/sdevolder/codestory2013/server/grizzly/GrizzlyServer.java
+++ codestory2013/src/main/java/com/sdevolder/codestory2013/server/grizzly/GrizzlyServer.java
@@ -1,12 +1,15 @@
 package com.sdevolder.codestory2013.server.grizzly;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import com.sdevolder.codestory2013.server.AbstractServer;
+import com.sun.jersey.api.container.filter.LoggingFilter;
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
@@ -24,7 +27,12 @@ public class GrizzlyServer extends AbstractServer {
     public void launchServer() throws Exception {
 
         System.out.println("Starting grizzly...");
+
         ResourceConfig rc = new PackagesResourceConfig("com.sdevolder.codestory2013.server.grizzly.servlet");
+        Map<String, Object> entries = new HashMap<String, Object>();
+        entries.put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS, LoggingFilter.class.getName());
+        entries.put(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS, LoggingFilter.class.getName());
+        rc.setPropertiesAndFeatures(entries);
         httpServer = GrizzlyServerFactory.createHttpServer(getBaseURI(), rc);
         System.in.read();
     }
